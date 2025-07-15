05d08ad0ebc24d664ce7b4c7b13b1d604515e84b
Anshuljangir <anshuljangir2004@gmail.com>
2025-07-15 08:46:09 +0530

    added

---
+++ app.py
@@ -0,0 +1,26 @@
+
+from dotenv import load_dotenv 
+load_dotenv() 
+import streamlit as st 
+import os 
+import google.generativeai as genai 
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model = genai.GenerativeModel("gemini-2.0-flash") 
+
+def my_output(query):
+    response = model.generate_content(query) 
+    return response.text 
+
+#### UI Development using streamlit 
+
+st.set_page_config(page_title="Chat_bot")
+st.header("Chat_bot") 
+input = st.text_input("Input " , key = "input")  
+submit = st.button("Tap here for Solution") 
+
+if submit :
+    response = my_output(input) 
+    st.subheader("After deep thinking... here’s the answer:")
+    st.write(response)
